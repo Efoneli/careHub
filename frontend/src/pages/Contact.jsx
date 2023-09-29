fac88a37dfc1c32b4b656069451951825efef4f6
efoneli <abbaefoneli@gmail.com>
2023-09-29 12:47:48 +0100

    changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_EMAIL":
+      return { ...state, email: action.payload };
+    case "SET_SUBJECT":
+      return { ...state, subject: action.payload };
+    case "SET_MESSAGE":
+      return { ...state, message: action.payload };
+    case "SET_ERROR":
+      return { ...state, error: action.payload };
+    case "SET_SUBMITTED":
+      return { ...state, submitted: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Contact = () => {
+  const [state, dispatch] = useReducer(formReducer, {
+    message: "",
+    email: "",
+    subject: "",
+    submitted: null,
+    error: { email: "", subject: "", message: "" },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let tempError = { email: "", subject: "", message: "" };
+    if (!state.email) tempError.email = "Email is required";
+    if (!state.subject) tempError.subject = "Subject is required";
+    if (!state.message) tempError.message = "Message is required";
+    dispatch({ type: "SET_ERROR", payload: tempError });
+    if (state.email && state.subject && state.message && !tempError.email && !tempError.subject && !tempError.message) {
+      console.log(`Email: ${state.email}`);
+      console.log(`Subject: ${state.subject}`);
+      console.log(`Message: ${state.message}`);
+      dispatch({ type: "SET_EMAIL", payload: "" });
+      dispatch({ type: "SET_SUBJECT", payload: "" });
+      dispatch({ type: "SET_MESSAGE", payload: "" });
+      dispatch({ type: "SET_SUBMITTED", payload: true });
+      setTimeout(() => {
+        dispatch({ type: "SET_SUBMITTED", payload: false });
+      }, 3000);
+    }
+  };
+
   return (
     <section>
       <div className="px-4 mx-auto max-w-screen-md">
@@ -9,7 +55,7 @@ const Contact = () => {
           Got a technical issue? Want to send feedback about a beta feature? Let
           us know.
         </p>
-        <form action="#" className="space-y-8">
+        <form action="#" className="space-y-8" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email" className="form__label">
               Your Email
@@ -17,9 +63,11 @@ const Contact = () => {
             <input
               type="email"
               id="email"
-              placeholder="example@gmail.com"
+              value={state.email}
+              onChange={(e) => dispatch({ type: "SET_EMAIL", payload: e.target.value })}
               className="form__input mt-1"
             />
+            {state.error.email && <p style={{ color: "red" }}>{state.error.email}</p>}
           </div>
           <div>
             <label htmlFor="subject" className="form__label">
@@ -28,9 +76,11 @@ const Contact = () => {
             <input
               type="text"
               id="subject"
-              placeholder=""
+              value={state.subject}
+              onChange={(e) => dispatch({ type: "SET_SUBJECT", payload: e.target.value })}
               className="form__input mt-1"
             />
+            {state.error.subject && <p style={{ color: "red" }}>{state.error.subject}</p>}
           </div>
           <div className="sm:col-span-2">
             <label htmlFor="message" className="form__label">
@@ -41,12 +91,21 @@ const Contact = () => {
               rows="6"
               type="text"
               id="message"
-              placeholder="Leave a comment..."
+              value={state.message}
+              onChange={(e) => dispatch({ type: "SET_MESSAGE", payload: e.target.value })}
               className="form__input mt-1"
             />
-            <button type="submit" className="btn rounded sm:w-fit">Submit</button>
+            {state.error.message && <p style={{ color: "red" }}>{state.error.message}</p>}
+            <button type="submit" className="btn rounded sm:w-fit mb-5">Submit</button>
           </div>
         </form>
+        {state.submitted === true && (
+  <div className={`bg-green-100 border text-center border-green-400 text-green-700 px-4 py-3 rounded relative animate-fade-in`} role="alert">
+    <strong className="font-bold">Success!</strong>
+    <span className="block sm:inline"> Your feedback has been submitted. We appreciate your input and will look into your issue promptly. Thank you for helping us improve our services.</span>
+  </div>
+)}
+
       </div>
     </section>
   );
--- frontend/src/pages/ServiceMoreDetail.jsx
+++ frontend/src/pages/ServiceMoreDetail.jsx
@@ -10,27 +10,28 @@ const ServiceMoreDetail = () => {
     return <p>Service not found</p>;
   }
 
-  const { history, image, cure, doctors } = service;
+  const { about, image, cure, doctors } = service;
 
   return (
+    <div className="container">
     <div className="service-detail p-5 bg-gray-100">
-      <h2 className="heading text-center">
+      <h2 className="heading text-center mt-[30px] mb-[20px]">
         {name}
       </h2>
-      <h3 className="text-[26px] leading-9 text-headingColor font-[700] ">History</h3>
-      <p className="text-gray-500 text-lg ">{history}</p>
+      <h3 className="text-[26px] leading-9 text-headingColor font-[700] mb-[20px]">About</h3>
+      <p className="text-gray-500 text-lg mb-[20px]">{about}</p>
 
-      <img src={image} alt={name} className="max-w-full h-auto mx-auto block rounded-lg m-5" />
-      <h3 className="text-[26px] leading-9 text-headingColor font-[700] ">Methods</h3>
-      <p className="text-gray-500 text-lg">{cure.description}</p>
+      <img src={image} alt={name} className="max-w-full h-auto mx-auto block rounded-lg mb-[20px]" />
+      <h3 className="text-[26px] leading-9 text-headingColor font-[700] mb-[20px]">Methods</h3>
+      <p className="text-gray-500 text-lg mb-[20px]">{cure.description}</p>
       {cure.methods.map((method, index) => (
         <div key={index}>
-          <h4 className="text-[20px] leading-7 text-headingColor font-[600]">{method.name}</h4>
-          <p className="text-gray-500 text-lg">{method.details}</p>
+          <h4 className="text-[20px] leading-7 text-headingColor font-[600] mb-[20px]">{method.name}</h4>
+          <p className="text-gray-500 text-lg mb-[20px]">{method.details}</p>
         </div>
       ))}
-      <p className="text-gray-500 text-lg">{cure.note}</p>
-      <h3 className="text-[26px] leading-9 text-headingColor font-[700]">Doctors in the Field</h3>
+      <p className="text-gray-500 text-lg mb-[20px]">{cure.note}</p>
+      <h3 className="text-[26px] leading-9 text-headingColor font-[700] mb-[20px]">Doctors in the Field</h3>
       <ul className="list-none p-0">
         {doctors.map((doctor) => (
           <li key={doctor} className="text-gray-500 text-lg">
@@ -38,6 +39,7 @@ const ServiceMoreDetail = () => {
           </li>
         ))}
       </ul>
+    </div>
     </div>
   );
 };
